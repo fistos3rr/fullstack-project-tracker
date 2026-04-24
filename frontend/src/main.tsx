d5a71c091953cd6b98f8ab7405ea6a6566f1b5a3
fistos3rr <fistos3rr@gmail.com>
2026-04-25 01:42:04 +0300

    feat: docker-compose completed

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,9 @@
-// src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 import axios from 'axios';
 
-// Vite использует import.meta.env, Create React App - process.env
 axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000';
 
 const queryClient = new QueryClient();
